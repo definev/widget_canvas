15c71485016fb5cf0d2b4df98c258b2c9031e5aa
Definev <daiduong.workmail@gmail.com>
2023-12-14 23:00:49 +0700

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,14 +24,16 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  final elements = <CanvasElement>[] //
+  final elements = <CanvasElement<int>>[] //
       .binaryList(WidgetCanvasChildDelegate.defaultCompare(Axis.horizontal))
       .toValueNotifier();
+
+  final snap = ValueNotifier(false);
 
   @override
   void initState() {
     super.initState();
-    elements.value = <CanvasElement>[
+    elements.value = <CanvasElement<int>>[
       for (int i = 0; i < 10; i += 1) CanvasElement(Offset(i * 100, i * 100), id: i, data: i),
     ].binaryList(WidgetCanvasChildDelegate.defaultCompare(Axis.horizontal));
     elements.addListener(() => setState(() {}));
@@ -55,25 +57,30 @@ class _HomeViewState extends State<HomeView> {
             child: WidgetCanvas(
               clipBehavior: Clip.none,
               diagonalDragBehavior: DiagonalDragBehavior.weightedEvent,
-              delegate: WidgetCanvasChildDelegate(
+              delegate: WidgetCanvasChildDelegate<int>(
                 showGrid: true,
                 elements: elements.value,
-                builder: (context, element) => MovableCanvasElement(
-                  snap: true,
-                  element: element,
-                  elements: elements,
-                  child: const Text(''),
+                builder: (context, element) => ListenableBuilder(
+                  listenable: snap,
+                  builder: (context, __) => MovableCanvasElement<int>(
+                    snap: snap.value,
+                    element: element,
+                    elements: elements,
+                    child: Card(
+                      margin: EdgeInsets.zero,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('${element.id} \n Coor: ${element.coordinate} \n Data: ${element.data}'),
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
           ),
-          FilledButton(
-            onPressed: () {
-              for (final element in elements.value.list) {
-                print('${element.id} ${element.coordinate}');
-              }
-            },
-            child: const Text('log elements'),
+          ElevatedButton(
+            onPressed: () => snap.value = !snap.value,
+            child: Text(snap.value ? 'Snap' : 'Unsnap'),
           ),
         ],
       ),
