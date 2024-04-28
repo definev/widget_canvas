4758af68b9d94af8acc814deb110dccc78c13d65
Definev <daiduong.workmail@gmail.com>
2024-04-28 22:37:52 +0700

    fix equal function and add layer control

--- lib/src/domain/canvas_element.dart
+++ lib/src/domain/canvas_element.dart
@@ -8,22 +8,43 @@ class CanvasElement<T> extends ValueNotifier<Coordinate> implements Comparable<C
     super._value, {
     required this.id,
     required this.data,
+    this.layer,
   });
 
   final int id;
   final T data;
+  final int? layer;
+
   Coordinate get coordinate => value;
   set coordinate(Coordinate coordinate) => value = coordinate;
 
-  late final ChildVicinity vicinity =
-      ChildVicinity(xIndex: id, yIndex: WidgetCanvasRenderTwoDimensionalViewport.contentLayer);
+  bool _isSelected = false;
+  bool get isSelected => _isSelected;
+  set isSelected(bool value) {
+    if (_isSelected == value) return;
+    _isSelected = value;
+    notifyListeners();
+  }
+
+  late final ChildVicinity vicinity = ChildVicinity(
+    xIndex: id,
+    yIndex: layer ?? WidgetCanvasRenderTwoDimensionalViewport.contentLayer,
+  );
 
   @override
-  bool operator ==(Object other) => other is CanvasElement ? value == other.value : false;
+  bool operator ==(Object other) =>
+      other is CanvasElement ? value == other.value && id == other.id && layer == other.layer : false;
 
   @override
   int get hashCode => id;
 
   @override
   int compareTo(CanvasElement<Object?> other) => id.compareTo(other.id);
+
+  CanvasElement<E> cast<E>() => CanvasElement(
+        value,
+        id: id,
+        data: data as E,
+        layer: layer,
+      );
 }
